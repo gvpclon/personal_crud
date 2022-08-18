e61531296f86430066abc78086d42b7ae8845c76
german.vidaurre <german.vidaurre@impuestos.gob.bo>
2022-08-18 17:07:58 -0400

    18_08_2022 conexion base

---
+++ config/conexion.php
@@ -0,0 +1,20 @@
+<?php
+class Conexion{
+    protected $dbh;
+
+    protected function Conectar(){
+        try{
+            $conectar = $this->dbh= new PDO("mysql:local=localhost;dbname=crud","root","");
+            return $conectar;
+        }catch(Exception $e){
+            print "Error BD!!! ".$e->getMessage()."<br/>";
+            die();
+        }
+    }
+
+    public function set_names()
+    {
+        return $this->dbh->query("SET NAMES 'utf8'");
+    }
+}
+?>
--- view/MntProducto/index.php
+++ view/MntProducto/index.php
@@ -35,7 +35,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                <div class="sidebar-brand-text mx-3">Facturación <sup>Soporte</sup></div>
             </a>
 
             <!-- Divider -->
@@ -43,9 +43,9 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="../../index.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
+                    <span>Inicio</span></a>
             </li>
 
             <!-- Divider -->
@@ -53,7 +53,7 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Interface
+                Herramientas
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
@@ -61,13 +61,12 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
+                    <span>Certificado</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
+                    <div class="bg-white py-2 collapse-inner rounded">                      
+                        <a class="collapse-item" href="buttons.html">Crear</a>
+                        <a class="collapse-item" href="cards.html">Buscar</a>
                     </div>
                 </div>
             </li>
@@ -77,16 +76,13 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
+                    <span>Mantenimiento</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
+                        <a class="collapse-item" href="../MntProducto">Mantenimiento</a>
+                        <a class="collapse-item" href="utilities-border.html">Borders</a>                        
                     </div>
                 </div>
             </li>
@@ -96,7 +92,7 @@
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Addons
+                Cotingencia
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
@@ -120,28 +116,6 @@
                     </div>
                 </div>
             </li>
-
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-chart-area"></i>
-                    <span>Charts</span></a>
-            </li>
-
-            <!-- Nav Item - Tables -->
-            <li class="nav-item">
-                <a class="nav-link" href="tables.html">
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Tables</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
 
         </ul>
         <!-- End of Sidebar -->
