18e0434d8eac2fb3b1685fbc443794f40bae2a1f
german.vidaurre <german.vidaurre@impuestos.gob.bo>
2022-08-19 10:02:59 -0400

    19_08_2022 conexion

--- config/conexion.php
+++ config/conexion.php
@@ -2,9 +2,20 @@
 class Conexion{
     protected $dbh;
 
-    protected function Conectar(){
+    protected function ConectarMySql(){
         try{
             $conectar = $this->dbh= new PDO("mysql:local=localhost;dbname=crud","root","");
+            return $conectar;
+        }catch(Exception $e){
+            print "Error BD!!! ".$e->getMessage()."<br/>";
+            die();
+        }
+    }
+
+    protected function ConectarPostgreSql(){
+        try{
+            //modificar
+            $conectar = $this->dbh= new PDO("postgresql:local=localhost;dbname=crud","root","");
             return $conectar;
         }catch(Exception $e){
             print "Error BD!!! ".$e->getMessage()."<br/>";
